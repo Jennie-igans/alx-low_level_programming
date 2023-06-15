5c0c5e21242ef056b58d8457a174d701ae56d4ae
Jennie-igans <jennyigans23.jx@gmail.com>
2023-06-15 16:43:49 -0700

    C 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly and that piece of art is useful" - Dora Korpar
- * ,2015-10-19, follwoed by a new line, to the standard error
+ * main - prints exactly and that piece of art is useful" - Dora Kor
+ * par, 2015-10-19, follwoed by a new line, to the standard error
  * Return: (1)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar
-		       ,2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korp
+ar, 2015-10-19\n", stdout);
+return (1);
 }
