5a63faa046cc931e94ab352d4a08a55ff1460d2d
Jennie-igans <jennyigans23.jx@gmail.com>
2023-06-15 13:25:41 -0700

    C 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prins "with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ * Return: Always 0
+ * 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
