b2c7084f3cb64f0f09bdc6b4174acef3a2e5ee5a
Jennie-igans <jennyigans23.jx@gmail.com>
2023-06-15 14:20:54 -0700

    C 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("size of int data types in C: %2d bytes \n", sizeof(short int));
-	printf("size of long int data types in C: %2d bytes \n", sizeof(long int));
-	printf("size of float data types in C: %2d bytes \n", sizeof(float));
-	printf("size of double data types in C: %2d bytes \n", sizeof(double));
-	printf("size of long double data types in C: %2d bytes \n", sizeof(long double));
-	printf("size of char data types in C: %2d bytes \n", sizeof(char));
+	printf("size of int data types in C: %zu bytes\n", sizeof(short int));
+	printf("size of long int data types in C: %zu bytes\n", sizeof(long int));
+	printf("size of float data types in C: %zu bytes\n", sizeof(float));
+	printf("size of long long int data types in C: %zu bytes\n", sizeof(long long int));
+	printf("size of char data types in C: %zu bytes\n", sizeof(char));
 	return (0)
 }
