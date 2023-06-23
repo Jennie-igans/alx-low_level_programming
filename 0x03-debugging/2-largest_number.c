facfd917807631bd48c1d1310a31affb94702405
Jennie-igans <jennyigans23.jx@gmail.com>
2023-06-23 16:16:59 -0700

    debugging 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (a > b && c > a)
-    {
-        largest = c;
-    }
-    else if (b > c);
-    {
-        largest = b;
-    }
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+largest = b;
+}
 
-    return (largest);
+return (largest);
 }
