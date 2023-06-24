aba135df4d11e6be59d8f555c9e62c8f439cf1c5
Jennie-igans <jennyigans23.jx@gmail.com>
2023-06-23 21:25:21 -0700

    nested loops 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,12 +13,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n *= 0;
+		n *= -1;
 		k = n;
 		_putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
-	print_number (k);
+	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
 }
