18d11e3e868225c00dbaba51bd51eac4dfb4ce46
Jennie-igans <jennyigans23.jx@gmail.com>
2023-07-03 08:02:23 -0700

    0x07-pointers_arrays_strings 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-	int j = n;
+	int j = 0;
+	int i = n;
 
-	for (; i > j; i++)
+	for (; j < i; j++)
 	{
-		dest[i] = src[i];
+		dest[j] = src[j];
 		n--;
 	}
 	return (dest);
